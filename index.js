df03401063cda19235cb165ebfb86f9271a8bd51
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2020-02-02 06:31:53 +0100

    Update 90 + Quick-load

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ String.prototype.clr = function(hexColor) { return `<font color='#${hexColor}'>$
 
 module.exports = function EZChanCmd(mod) {
 
+	let maxDistance = 5000,		// Distance at which quick-load will always ignore loading screens
+	longTele = true,		// Enables quick-load for long teleports beyond maxDistance
+	longTeleHoldMs = 500	// Hold duration to prevent falling through the map - Depends on your disk speed
+
 	const channels = {'2': 1, '3': 1, '5': 3, '13': 8, '7001': 7, '7002': 3, '7003': 5, '7004': 4, '7005': 10, '7011': 7, '7012': 5, '7013': 1, '7014': 5, '7015': 7, '7021': 3, '7022': 3, '7023': 4, '7031': 3, '8001': 1};
 
 	let hooks = [],
 		sCurrentChannel = null,
-		skip = false,
+		seconds = 0,
 		sSpawnMe = null,
 		cOne = -1,
-		cTwo = -1;
+		cTwo = -1,
+		zone = -1,
+		serverQuick = false,
+		modifying = false,
+		myPos = null,
+		spawnLoc = null;
+
+		mod.hook('S_LOGIN', 'raw', () => {
+			zone = -1
+			myPos = null
+		})
 
 	mod.game.on('enter_game', () => {
-		if (!mod.manager.isInstalled('quick-load'))
 			load();
 	});
 
@@ -78,21 +91,21 @@ module.exports = function EZChanCmd(mod) {
 	function load() {
 		if (!hooks.length) {
 
-			hook('S_PREPARE_SELECT_CHANNEL', 1, () => { skip = true; });
+			hook('S_PREPARE_SELECT_CHANNEL', 1, () => { seconds = 0; });
 
-			hook('S_CANCEL_SELECT_CHANNEL', 1, () => { skip = false; });
+			hook('S_CANCEL_SELECT_CHANNEL', 1, () => { unk = 0; });
 
 			hook('S_LOAD_TOPO', 3, {order: 100}, (event) => {
-				if(skip)
+				if(seconds)
 					return event.quick = true;
-				skip = false;
+					seconds = 0;
 			});
 
 			hook('S_SPAWN_ME', 3, {order: 100}, (event) => {
 				sSpawnMe = event;
 
-				if(skip) {
-					skip = false;
+				if(seconds) {
+					seconds = 0;
 					mod.send('S_SPAWN_ME', 3, (event));
 					mod.send('C_PLAYER_LOCATION', 5, {
 						loc: event.loc,
@@ -123,6 +136,61 @@ module.exports = function EZChanCmd(mod) {
 			});
 		}
 	}
+
+	mod.hook('S_LOAD_TOPO', 3, {order: 100}, event => {
+		serverQuick = event.quick
+
+		if(event.zone === zone && (longTele || myPos.dist3D(event.loc) <= maxDistance))
+			return event.quick = modifying = true
+
+		myPos = event.loc
+		zone = event.zone
+		modifying = false
+	})
+
+	mod.hook('S_SPAWN_ME', 3, {order: 100}, event => {
+		if(!serverQuick) spawnLoc = event
+
+		if(modifying) {
+			if(!myPos || myPos.dist3D(event.loc) > maxDistance)
+				process.nextTick(() => { mod.send('S_ADMIN_HOLD_CHARACTER', 2, {hold: true}) })
+			else modifying = false
+
+			mod.send('S_SPAWN_ME', 3, event) // Bring our character model back from the void
+			mod.send('C_PLAYER_LOCATION', 5, { // Update our position on the server
+				loc: event.loc,
+				dest: event.loc,
+				type: 7
+			})
+		}
+	})
+
+	mod.hook('S_ADMIN_HOLD_CHARACTER', 'raw', () => !modifying && undefined)
+
+	mod.hook('C_PLAYER_LOCATION', 5, event => {
+		if(spawnLoc) {
+			// Did we accidentally spawn under the map? Let's fix that!
+			if(event.loc.z !== spawnLoc.loc.z) {
+				mod.send('S_INSTANT_MOVE', 3, spawnLoc)
+				spawnLoc = null
+				return false
+			}
+			spawnLoc = null
+		}
+	})
+
+	mod.hook('C_PLAYER_LOCATION', 5, {order: 100, filter: {fake: null}}, event => { myPos = event.loc })
+
+	mod.hook('C_VISIT_NEW_SECTION', 'raw', () => {
+		// If our client doesn't send C_PLAYER_LOCATION before this packet, then it's most likely user input
+		spawnLoc = null
+
+		if(modifying) {
+			mod.setTimeout(() => { mod.send('S_ADMIN_HOLD_CHARACTER', 2, {hold: false}) }, longTeleHoldMs)
+			modifying = false
+		}
+	})
+
 
 	function unload() {
 		if (hooks.length) {
